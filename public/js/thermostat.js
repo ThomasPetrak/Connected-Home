aba5497ae14e6792004c6cf19ed3af76df56830c
Tomas Petrak <tomaspetrak@Tomass-MacBook-Pro.local>
2017-04-06 10:49:36 +0200

    minor fixes

--- public/js/thermostat.js
+++ public/js/thermostat.js
@@ -1,11 +1,5 @@
-function switchLight(id) {
-    if(document.getElementById(id).src.indexOf("img/lightOn.png") != -1) {
-        document.getElementById(id).src="img/lightOff.png";
-        sendJson(id);
-        alert(id);
-    } else{
-        document.getElementById(id).src="img/lightOn.png";
-        sendJson(id);
-    }
-}
+$(document).ready(function(){
+    alert($("#temp1 span").text());
+});
+
 
